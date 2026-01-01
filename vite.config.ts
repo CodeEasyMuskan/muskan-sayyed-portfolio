1474b28bca8411bfee8807eeca9470ebdd140e43
MuskanSarfaraz <muskansyd86@gmail.com>
2026-01-02 03:40:31 +0530

    Fix Vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/muskan-sayyed-portfolio/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
